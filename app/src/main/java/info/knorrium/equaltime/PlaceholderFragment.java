077c1bca7b62bdb586e103c97ebe421df6cf3e0b
Felipe Knorr Kuhn <git@knorrium.info>
2015-04-03 23:51:36 -0700

    instantiate the button and the clickListener to start/stop the timer

--- app/src/main/java/info/knorrium/equaltime/PlaceholderFragment.java
+++ app/src/main/java/info/knorrium/equaltime/PlaceholderFragment.java
@@ -3,12 +3,16 @@ package info.knorrium.equaltime;
 import android.content.Context;
 import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.Chronometer;
 import android.widget.TextView;
+import android.widget.Toast;
 
 /**
  * Created by felipek on 4/3/15.
@@ -28,6 +32,29 @@ public class PlaceholderFragment extends Fragment {
         TextView name2view = (TextView) rootView.findViewById(R.id.txtName2);
         String strName2 = prefs.getString(context.getString(R.string.pref_second_name_key), context.getString(R.string.pref_second_name_default));
         name2view.setText(strName2);
+
+        Button btnTimer1 = (Button) rootView.findViewById(R.id.btnTimer1);
+
+        btnTimer1.setOnClickListener( new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Button button = (Button) v;
+                View rootView = v.getRootView();
+                Chronometer timer1 = (Chronometer) rootView.findViewById(R.id.chronometer1);
+
+                if (button.getText().equals("Start")) {
+                    timer1.setBase(SystemClock.elapsedRealtime());
+                    timer1.start();
+
+                    Toast.makeText(getActivity().getApplicationContext(), "Timer started", Toast.LENGTH_SHORT).show();
+                    button.setText("Stop");
+                } else {
+                    Toast.makeText(getActivity().getApplicationContext(), "Timer stopped", Toast.LENGTH_SHORT).show();
+                    button.setText("Start");
+                    timer1.stop();
+                }
+            }
+        });
 
         return rootView;
     }
