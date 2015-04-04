92bb71a973f14ddd552bb3d17352a03cf5d4a7f8
Felipe Knorr Kuhn <git@knorrium.info>
2015-04-04 01:24:17 -0700

    make the fragment load data using the adapter

--- app/src/main/java/info/knorrium/equaltime/PlaceholderFragment.java
+++ app/src/main/java/info/knorrium/equaltime/PlaceholderFragment.java
@@ -10,23 +10,118 @@ import android.os.Bundle;
 import android.os.SystemClock;
 import android.preference.PreferenceManager;
 import android.support.v4.app.Fragment;
+import android.support.v4.app.LoaderManager;
+import android.support.v4.content.CursorLoader;
+import android.support.v4.content.Loader;
 import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.Chronometer;
+import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.util.List;
 
 import info.knorrium.equaltime.data.TimeTableContract;
 
 /**
  * Created by felipek on 4/3/15.
  */
-public class PlaceholderFragment extends Fragment {
+public class PlaceholderFragment extends Fragment implements LoaderManager.LoaderCallbacks<Cursor> {
+    private static final int EVENT_LOADER = 0;
+    private TimeTableAdapter mTimeTableAdapter;
+
+    private static final String[] EVENT_COLUMNS = {
+            TimeTableContract.EventEntry.TABLE_NAME + "." + TimeTableContract.EventEntry._ID,
+            TimeTableContract.EventEntry.COLUMN_EVENT_DATE,
+            TimeTableContract.EventEntry.COLUMN_EVENT_COORD_LAT,
+            TimeTableContract.EventEntry.COLUMN_EVENT_COORD_LONG,
+            TimeTableContract.EventEntry.COLUMN_EVENT_DURATION,
+            TimeTableContract.EventEntry.COLUMN_EVENT_CREATOR
+    };
+
+    static final int COL_EVENT_ID = 0;
+    static final int COL_EVENT_DATE = 1;
+    static final int COL_EVENT_COORD_LAT = 2;
+    static final int COL_EVENT_COORD_LONG = 3;
+    static final int COL_EVENT_DURATION = 4;
+    static final int COL_EVENT_CREATOR = 5;
+
+
+    @Override
+    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
+
+//        String locationSetting = Utility.getPreferredLocation(getActivity());
+
+        // Sort order:  Ascending, by date.
+        String sortOrder = TimeTableContract.EventEntry.COLUMN_EVENT_DATE + " ASC";
+//        Uri weatherForLocationUri = TimeTableContract.EventEntry.buildEventUri(1);
+        Uri contenturi = Uri.parse("content://info.knorrium.equaltime");
+        Uri tableuri = Uri.withAppendedPath(contenturi,TimeTableContract.EventEntry.TABLE_NAME);
+
+        return new CursorLoader(getActivity(), tableuri, EVENT_COLUMNS, null, null, sortOrder);
+    }
+
+    public void onLoadFinished(Loader<Cursor> cursorLoader, Cursor cursor) {
+        mTimeTableAdapter.swapCursor(cursor);
+    }
+
+    public void onLoaderReset(Loader<Cursor> cursorLoader) {
+        mTimeTableAdapter.swapCursor(null);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceStates){
+        super.onCreate(savedInstanceStates);
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+//        inflater.inflate(R.menu.forecastfragment, menu);
+    }
+
+    @Override
+    public void onActivityCreated(Bundle savedInstanceState) {
+        getLoaderManager().initLoader(EVENT_LOADER, null, this);
+        super.onActivityCreated(savedInstanceState);
+    }
+
+    // since we read the location when we create the loader, all we need to do is restart things
+    void onLocationChanged( ) {
+//        updateWeather();
+        getLoaderManager().restartLoader(EVENT_LOADER, null, this);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+//        if (id == R.id.action_refresh) {
+//            updateWeather();
+//            return true;
+//        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+        mTimeTableAdapter = new TimeTableAdapter(getActivity(), null, 0);
+
         View rootView = inflater.inflate(R.layout.fragment_main, container, false);
         Context context = getActivity().getApplicationContext();
         SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(context);
@@ -40,6 +135,9 @@ public class PlaceholderFragment extends Fragment {
         name2view.setText(strName2);
 
         Button btnTimer1 = (Button) rootView.findViewById(R.id.btnTimer1);
+
+        ListView listView = (ListView) rootView.findViewById(R.id.listEntries1);
+        listView.setAdapter(mTimeTableAdapter);
 
         btnTimer1.setOnClickListener( new View.OnClickListener() {
             @Override
