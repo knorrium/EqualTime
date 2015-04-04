0433813a1fb4571074bcf1666de1e1bb6dc47f8a
Felipe Knorr Kuhn <git@knorrium.info>
2015-04-03 22:58:50 -0700

    extract the placeholderfragment into its own class

---
+++ app/src/main/java/info/knorrium/equaltime/PlaceholderFragment.java
@@ -0,0 +1,19 @@
+package info.knorrium.equaltime;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by felipek on 4/3/15.
+ */
+public class PlaceholderFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_main, container, false);
+        return rootView;
+    }
+}
