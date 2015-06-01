b31842a2e1a4d4af5de857c8eef01fdf7bb10f8d
Felipe Knorr Kuhn <git@knorrium.info>
2015-05-31 22:23:06 -0700

    comment out the id and lat long details for now

--- app/src/main/java/info/knorrium/equaltime/TimeTableAdapter.java
+++ app/src/main/java/info/knorrium/equaltime/TimeTableAdapter.java
@@ -7,26 +7,32 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.TextView;
 
+import com.google.android.gms.maps.*;
+import com.google.android.gms.maps.MapFragment;
+import com.google.android.gms.maps.model.LatLng;
+
 /**
  * Created by felipek on 4/4/15.
  */
 public class TimeTableAdapter extends CursorAdapter {
 
     public static class ViewHolder {
-        public final TextView entryIdView;
+//        public final TextView entryIdView;
         public final TextView creatorView;
         public final TextView dateView;
         public final TextView timespentView;
-        public final TextView latitudeView;
-        public final TextView longitudeView;
+//        public final TextView latitudeView;
+//        public final TextView longitudeView;
+        public final TextView placeView;
 
         public ViewHolder(View view) {
-            entryIdView = (TextView) view.findViewById(R.id.entryIdView);
+//            entryIdView = (TextView) view.findViewById(R.id.entryIdView);
             creatorView = (TextView) view.findViewById(R.id.creatorView);
             dateView = (TextView) view.findViewById(R.id.dateView);
             timespentView = (TextView) view.findViewById(R.id.timespentView);
-            latitudeView = (TextView) view.findViewById(R.id.latitudeView);
-            longitudeView = (TextView) view.findViewById(R.id.longitudeView);
+//            latitudeView = (TextView) view.findViewById(R.id.latitudeView);
+//            longitudeView = (TextView) view.findViewById(R.id.longitudeView);
+            placeView = (TextView) view.findViewById(R.id.placeView);
         }
     }
 
@@ -51,12 +57,14 @@ public class TimeTableAdapter extends CursorAdapter {
     public void bindView(View view, Context context, Cursor cursor) {
         ViewHolder viewHolder = (ViewHolder) view.getTag();
 
-        viewHolder.entryIdView.setText(cursor.getString(PlaceholderFragment.COL_EVENT_ID));
-        viewHolder.latitudeView.setText(cursor.getString(PlaceholderFragment.COL_EVENT_COORD_LAT));
-        viewHolder.longitudeView.setText(cursor.getString(PlaceholderFragment.COL_EVENT_COORD_LONG));
+//        viewHolder.entryIdView.setText(cursor.getString(PlaceholderFragment.COL_EVENT_ID));
+//        viewHolder.latitudeView.setText(cursor.getString(PlaceholderFragment.COL_EVENT_COORD_LAT));
+//        viewHolder.longitudeView.setText(cursor.getString(PlaceholderFragment.COL_EVENT_COORD_LONG));
         viewHolder.dateView.setText(cursor.getString(PlaceholderFragment.COL_EVENT_DATE));
         viewHolder.timespentView.setText(cursor.getString(PlaceholderFragment.COL_EVENT_DURATION));
         viewHolder.creatorView.setText(cursor.getString(PlaceholderFragment.COL_EVENT_CREATOR));
+        viewHolder.placeView.setText(cursor.getString(PlaceholderFragment.COL_EVENT_TITLE));
+//        viewHolder.liteMapView.getMap().moveCamera(CameraUpdateFactory.newLatLngBounds(new LatLng(PlaceholderFragment.COL_EVENT_COORD_LAT, PlaceholderFragment.COL_EVENT_COORD_LONG),0));
 
     }
 }
