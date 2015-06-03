c7deb16a3b78d3117a22be2f4ae05a38c1a4bd07
Felipe Knorr Kuhn <git@knorrium.info>
2015-06-02 22:06:15 -0700

    rename the database (hi Sunshine App!)

--- app/src/main/java/info/knorrium/equaltime/data/TimeTableDbHelper.java
+++ app/src/main/java/info/knorrium/equaltime/data/TimeTableDbHelper.java
@@ -12,7 +12,7 @@ public class TimeTableDbHelper extends SQLiteOpenHelper {
 
     private static final int DATABASE_VERSION = 1;
 
-    static final String DATABASE_NAME = "weather.db";
+    static final String DATABASE_NAME = "events.db";
 
     public TimeTableDbHelper(Context context) {
         super(context, DATABASE_NAME, null, DATABASE_VERSION);
